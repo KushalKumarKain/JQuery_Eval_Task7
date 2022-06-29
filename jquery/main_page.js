3a9dc9828dceb231b071fd7e0a7d6e61a49bd176
Kushal Kumar Kain <85063289+KushalKumarKain@users.noreply.github.com>
2022-06-29 11:43:27 +0530

    changed the code to enable/disable action buttons

--- jquery/main_page.js
+++ jquery/main_page.js
@@ -1,33 +1,78 @@
-$(document).ready(function() {
-okayval= function(){
-  var inpval = $('#TextInput').val();
+// $(document).ready(function() {
+// okayval= function(){
+//   var inpval = $('#TextInput').val();
+//   if(val !== ""){
   
-    $('#sec_table tbody').append('<tr><td>' + inpval + 
-    '</td><td><button type="button" id="done" class="Done" name="DONE" >DONE</button>' +
-    '<button type="button" id="undone" class="UnDone" name="UNDONE" >UNDONE</button>' + 
-    '<button type="button" id="del" class="Delete" name="DELETE" >DELETE</button></td>' +
-    '<td id="status"></td></tr>');
-  }
-
-});
-
-
+//     $('#sec_table tbody').append('<tr><td>' + inpval + 
+//     '</td><td><button type="button" id="done" class="Done" name="DONE" >DONE</button>' +
+//     '<button type="button" id="undone" class="UnDone" name="UNDONE" >UNDONE</button>' + 
+//     '<button type="button" id="del" class="Delete" name="DELETE" >DELETE</button></td>' +
+//     '<td id="status"></td></tr>');
+//   }
+// }
+// });
 
 
-  $(document).on('click','.Done',function(){
-    $(this).parent().siblings("#status").html("&#9989;");
-  });
 
-  $(document).on('click','.UnDone',function(){
-    $(document).on('click','.Delete',function(){
-      var rcount=$("#sec_table tr").length;
-      if (rcount>2){
-        debugger
-      $(this).parent().parent().remove();}
+
+//   $(document).on('click','.Done',function(){
+//     $(this).parent().siblings("#status").html("&#9989;");
+//   });
+
+//   $(document).on('click','.UnDone',function(){
+//     $(document).on('click','.Delete',function(){
+//       let rcount=$("#tbody tr").length;
+//       debugger
+//       if (rcount>3 && $(this).parent().siblings("#status").html() ==="&#x274C;");{
+
+//       $(this).parent().parent().remove();}
+//     });
+
+//     $(this).parent().siblings("#status").html("&#x274C;");
+
+//   });
+
+  
+
+
+
+$('#ok').click(function(){
+    let val=$('#TextInput').val();
+
+    $('#TextInput').val("")
+    if(val !== ""){
+        $('#sec_table').append(
+          `<tr>
+                <td>${val}</td>
+                <td>
+                    <input type="button" value="done" class="Done">
+                    <input type="button" value="undone" class="UnDone">
+                    <input type="button" value="delete" class="Delete">
+                </td>
+                <td id ="status">  </td>
+                </tr>`
+          )
+    }
+
+    $('.Done').click(function(){
+        $(this).parent().siblings("#status").html("&#9989;");
     });
 
-    $(this).parent().siblings("#status").html("&#x274C;");
+    
+    
+    $('.UnDone').click(function(){
+        
+      $(this).parent().siblings("#status").html("&#x274C;");
+        
+    });
+    
+    $('.Delete').click(function(){
 
-  });
+        let tablength = $('#sec_table tr').length;
+        if(tablength > 2 && $(this).parent().siblings('#status').html() === '❌'){
+            $(this).parent().parent().remove();
+    }
+    })
 
-  
+
+});
